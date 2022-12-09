70ba976964ed127a639418cbcd6f7a751e85842d
Jackson Saenz <saenzjack1@icloud.com>
2022-12-09 03:09:06 -0600

    added drop down, top movers?

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import axios from 'axios';
 
 class App extends React.Component {
   state = {
-    ticker: '',
+    ticker: 'BTC',
     price: null,
-    name: null
+    name: null,
+    tickers: ['BTC', 'ETH', 'XRP', 'BCH', 'ADA', 'XLM', 'NEO', 'LTC', 'EOS', 'XEM', 'IOTA', 'DASH', 'XMR', 'TRX', 'ICX', 'ETC', 'QTUM', 'BTG', 'VET', 'LSK', 'USDT', 'OMG', 'STEEM', 'ZEC', 'SC', 'BNB', 'XVG', 'ZRX', 'REP', 'WAVES', 'MKR', 'DCR', 'BAT', 'DGB', 'LRC', 'KNC', 'SYS', 'BNT', 'REQ', 'LINK', 'QSP', 'CVC', 'RLC', 'ENJ', 'STORJ', 'ANT', 'SNGLS', 'THETA', 'MANA', 'MLN', 'DNT', 'AMP', 'NMR', 'STX', 'GMT', 'POLIS', 'DOT', 'DAI', 'UNI', 'ATOM', 'GRT', 'LUNA', 'SCRT', 'IMX', 'ZIL', 'XTZ', 'FIL', 'OP', 'NANO', 'WBTC', 'BSV', 'DOGE', 'USDC', 'OXT', 'ALGO', 'BAND', 'BTT', 'FET', 'KAVA', 'USDP', 'PAXG', 'REN', 'AAVE', 'YFI', 'NU', 'MATIC', 'ICP', 'SOL', 'SUSHI', 'UMA', 'SNX', 'CRV', 'COMP', 'CELO', 'KSM', 'NKN', 'SHIB', 'SKL', 'SAND', 'UST', 'AVAX', 'IOTX', 'AXS', 'XYO', 'ANKR', 'CHZ', 'LPT', 'COTI', 'KEEP', 'GALA', 'CRO', 'ACHP', 'JASMY', 'SLP', 'APE', 'BUSD', 'CAKE', 'EGLD', 'ENS', 'FTM', 'FTT', 'HBAR', 'MBOX', 'MINA', 'MOVR', 'NEAR', 'NEXO', 'POLS', 'QNT', 'QUICK', 'RUNE', 'RVN', 'WAXP', 'WRX', 'XEC', 'CEL', 'ALPACA', 'AUDIO', 'AVA', 'CHR', 'CKB', 'CLV', 'FARM', 'FLOW', 'GLMR', 'IDEX', 'INJ', 'JOE', 'MIR', 'POLY', 'PYR', 'RARE', 'RAY', 'ROSE', 'SFP', 'SRM', 'STMX', 'SUN', 'SXP', 'VGX', 'WOO', 'YGG', 'LUNC', 'APT', 'MASK', 'DYDX', 'CTSI', 'CVX', 'FORTH', 'LDO', 'METIS', 'RBN', 'SAMO', 'SPELL', 'DLCS', 'MTVS', 'DFX', 'SCPX', 'SCPXX', 'DIGS', 'CCYS', 'CCYX', 'CPUS', 'CNES', 'CMI', 'CMIX', 'SMT', 'CCY', 'CCX', 'DTZ', 'DCF', 'CPU', 'CNE']
   }
 
   handleChange = (event) => {
-    this.setState({ ticker: event.target.value });
+    // Get the value of the selected <option> element
+    const ticker = event.target.options[event.target.selectedIndex].value;
+    // Update the state with the selected value
+    this.setState({ ticker });
   }
 
   handleSubmit = (event) => {
@@ -48,7 +52,11 @@ class App extends React.Component {
       <form onSubmit={this.handleSubmit}>
         <label>
           Ticker symbol:
-          <input type="text" value={this.state.ticker} onChange={this.handleChange} />
+          <select value={this.state.ticker} onChange={this.handleChange}>
+            {this.state.tickers.map(ticker => (
+              <option value={ticker}>{ticker}</option>
+            ))}
+          </select>
         </label>
         <input type="submit" value="Submit" />
         {this.state.price && <p>The price of {this.state.name} is: {this.state.price}</p>}
